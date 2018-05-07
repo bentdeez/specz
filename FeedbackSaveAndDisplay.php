6a0639afbcf9fc93249fea539a60aed9d651d125
JB <36246363+bentdeez@users.noreply.github.com>
2018-05-07 13:00:24 -0700

    Add files via upload

---
+++ FeedbackSaveAndDisplay.php
@@ -0,0 +1,34 @@
+<html xmlns = "http://www.w3.org/1999/xhtml">
+
+<head>
+<title>feedback List</title>
+
+<style type = "text/css">
+body {
+font-family: tahoma, helvetica, arial, sans-serif;
+}
+table, tr, td, th {
+text-align: center;
+font-size: .9em;
+border: 3px groove;
+padding: 5px;
+background-color: #dddddd;
+}
+</style>
+</head>
+
+<body>
+<p style = "font-size: 2em;">feedback List</p>
+
+<table>
+<thead>
+<tr>
+<th style = "width: 100px;">Name</th>
+<th style = "width: 200px;">Feedback</th>
+<th style = "width: 50px;">Score</th>
+<th style = "width: 100px;">Product</th>
+</tr>
+</thead>
+
+<tbody>
+Could not insert into database: You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near ' '')' at line 1
